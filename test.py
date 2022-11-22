96c32fa0e4624ef5959625688eb206bb2ab50e36
Katrin1405 <110610865+Katrin1405@users.noreply.github.com>
2022-11-22 18:06:47 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,19 @@
 import pytest
 from app.Calculator import Calculator
 
+
 class TestCalc:
     def setup(self):
         self.calc = Calculator
- # def teardown(self):
- #    self.calc = Calculator
- #
- # def sum(x,y):
- #    return x+y
- #
- # def rest_sum():
- #    assert sum (1,2) == 3
 
-    def test_adding_success(self):
-        assert self.calc.adding(self, 1 , 1) == 2
+    def test_multiply_correct(self):
+        assert self.calc.multiply(self, 3, 8) == 24
 
-    def test_adding_unsuccess(self):
-        assert self.calc.adding(self, 1 , 1) == 3
+    def test_division_correct(self):
+        assert self.calc.division(self, 144, 12) == 12
 
-    def test_zero_devision(self):
-        with pytest.raises(ZeroDivisionError):
-            self.calc.division(self, 1 , 0)
+    def test_subtraction_correct(self):
+        assert self.calc.subtraction(self, 15, 3) == 12
 
-    def multiply_test(self):
-        assert self.calc.multiply(self, 1, 2 ) == 2
-
-    def subtraction_test(self):
-        assert self.calc.subtraction(self, 4, 2 ) == 1
-
-    def vstepen_test(self):
-        assert self.calc.vstepen(self, 2 , 2) == 4
-
-
-    def teardown(self):
-        print('Выполнение метода teardown')
+    def test_adding_correct(self):
+        assert self.calc.adding(self, 20, 10) == 30
